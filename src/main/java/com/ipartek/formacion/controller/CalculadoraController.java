05a58221be3db306fba4edae3b77bf7d2b40a1e8
Jose <jlhm2077@gmail.com>
2019-07-24 13:56:14 +0200

    Ejercicio calculadora

---
+++ src/main/java/com/ipartek/formacion/controller/CalculadoraController.java
@@ -0,0 +1,103 @@
+package com.ipartek.formacion.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ipartek.formacion.controller.pojo.Alert;
+
+/**
+ * Servlet implementation class CalculadoraController
+ */
+@WebServlet("/calculadora")
+public class CalculadoraController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public static final int OP_SUMAR = 1;
+    public static final int OP_RESTAR = 2;
+    public static final int OP_MULTIPLICAR = 3;
+    public static final int OP_DIVIDIR = 4;
+   
+    public static final String VIEW_INDEX = "ejemplos/calculadora.jsp";
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+		if (!isNumeric(request.getParameter("inputNumero1")) || !isNumeric(request.getParameter("inputNumero2"))) {
+			request.setAttribute("mensaje", new Alert("danger", "Zorrrooooo" ));
+		}else {
+		
+		
+			float numero1 = Float.parseFloat(request.getParameter("inputNumero1"));
+			float numero2 = Float.parseFloat(request.getParameter("inputNumero2"));
+			int inputOperacion = Integer.parseInt(request.getParameter("inputOperacion"));
+			
+			float resultado = 0f;
+			String mensaje = "-";
+			
+			try {
+				switch (inputOperacion) {
+				case OP_SUMAR:
+					resultado = numero1 + numero2;
+					break;
+				case OP_RESTAR:
+					resultado = numero1 - numero2;
+					break;
+				case OP_MULTIPLICAR:
+					resultado = numero1 * numero2;
+					break;
+				case OP_DIVIDIR:
+					if (numero1==0 || numero2==0)
+					{
+						request.setAttribute("mensaje", new Alert("warning", "Fuck you!" ));
+					}else {
+						resultado = numero1 / numero2;
+					}
+					
+					break;
+				default:
+					request.setAttribute("mensaje", new Alert("warning", "Ooopps" ));
+					break;
+				}
+			}catch(Exception e) {
+				request.setAttribute("mensaje", new Alert("danger", e.getMessage() ));
+			}
+			
+			
+			request.setAttribute("inputNumero1", numero1);
+			request.setAttribute("inputNumero2", numero2);
+			request.setAttribute("resultado", resultado);
+			
+			request.setAttribute("op", inputOperacion);
+		}
+		request.getRequestDispatcher(VIEW_INDEX).forward(request, response);
+	}
+	
+	public static boolean isNumeric(String cadena) {
+
+        boolean resultado;
+
+        try {
+            Integer.parseInt(cadena);
+            resultado = true;
+        } catch (NumberFormatException excepcion) {
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
+}
---
+++ src/main/webapp/ejemplos/calculadora.jsp
@@ -0,0 +1,49 @@
+<%@page import="com.ipartek.formacion.controller.CalculadoraController"%>
+<%@include file="../includes/header.jsp"%>
+<%@include file="../includes/navbar.jsp"%>
+
+<h1>Calculadora</h1>
+
+<%@include file="../includes/mensaje.jsp"%>
+
+<form action="calculadora" method="post">
+  <div class="form-group row">
+    <label for="inputNumero1" class="col-sm-2 col-form-label">Número 1</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="inputNumero1" step="0.01" name="inputNumero1" value="${inputNumero1}" placeholder="Número 1">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputNumero2" class="col-sm-2 col-form-label">Número 2</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="inputNumero2" step="0.01" name="inputNumero2" value="${inputNumero2}"  placeholder="Número 2">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputOperacion" class="col-sm-2 col-form-label">Operación </label>
+    <div class="col-sm-10">
+      <select id="inputOperacion" name="inputOperacion" class="form-control">
+        <option value="<%=CalculadoraController.OP_SUMAR%>" ${(op==CalculadoraController.OP_SUMAR)?'selected':''}>Sumar</option>
+        <option value="<%=CalculadoraController.OP_RESTAR%>" ${(op==CalculadoraController.OP_RESTAR)?'selected':''}>Restar</option>
+        <option value="<%=CalculadoraController.OP_MULTIPLICAR%>" ${(op==CalculadoraController.OP_MULTIPLICAR)?'selected':''}>Multiplicar</option>
+        <option value="<%=CalculadoraController.OP_DIVIDIR%>" ${(op==CalculadoraController.OP_DIVIDIR)?'selected':''}>Dividir</option>
+      </select>
+    </div>
+  </div>
+ 
+  <c:if test="${resultado!=null}">
+	  <div class="form-group row">
+	    <div class="col-sm-12">
+	      <input type="number" readonly class="form-control" id="resultado" name="resultado" value="${resultado}">
+	    </div>
+	  </div>
+  </c:if>
+  <div class="form-group row">
+    <div class="col-sm-12">
+      <input type="submit" class="form-control" id="inputSubmit" placeholder="Calcular">
+    </div>
+  </div>
+</form>
+
+
+<%@include file="../includes/footer.jsp"%>
